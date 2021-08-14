2bdde8a99a989c34fca6eaf73683ce27dca38a0f
mihon.testosterone <mihon.testosterone@gmail.com>
2021-08-14 08:57:27 +0300

    +

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -26,6 +26,7 @@ const Statistics = ({ title, stats }) => {
 }
 
 Statistics.propTypes = {
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
