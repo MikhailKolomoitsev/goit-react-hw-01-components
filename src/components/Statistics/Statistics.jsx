7bab0e68373d6976e43baa97f73dc558a9325aa2
mihon.testosterone <mihon.testosterone@gmail.com>
2021-08-14 15:43:41 +0300

    prettiered

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,38 +1,35 @@
-import styles from './Statistics.module.css'
-import PropTypes from 'prop-types'
-
+import styles from "./Statistics.module.css";
+import PropTypes from "prop-types";
 
 const Statistics = ({ title, stats }) => {
-    
-    function randomBg() {
-        const colors = ['color1', 'color2', 'color3', 'color4', 'color5', 'color6']
-        const index = Math.floor(Math.random() * colors.length)
-        return colors[index]
-    }
+  function randomBg() {
+    const colors = ["color1", "color2", "color3", "color4", "color5", "color6"];
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+  }
 
-    return (
-        <section class="statistic">
-            {title && <h2 class="title">{title}</h2>}
-            <ul class={styles.stats}>
-      {stats.map(item => (
-        <li key={item.id} class={styles[randomBg()]}>
-            <span class='label'>{item.label}</span>
-              <span class={styles.percentage}>{item.percentage}%</span>
-        </li>))}
-                
-  </ul>
-</section>
-    )
-}
+  return (
+    <section class="statistic">
+      {title && <h2 class="title">{title}</h2>}
+      <ul class={styles.stats}>
+        {stats.map((item) => (
+          <li key={item.id} class={styles[randomBg()]}>
+            <span class="label">{item.label}</span>
+            <span class={styles.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-    }),
+    })
   ),
 };
 
-
-export default Statistics
+export default Statistics;
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,44 +1,43 @@
-import styles from './TransactionHistory.module.css'
-import PropTypes from 'prop-types'
+import styles from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
 
 function firstUpper(string) {
-    return string[0].toUpperCase()+string.slice(1)
+  return string[0].toUpperCase() + string.slice(1);
 }
-const TransactionHistory = ({items}) => {
-    return (
-        <table class={styles.transactionTable}>
-  <thead>
-    <tr class={styles.tableHeader}>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+const TransactionHistory = ({ items }) => {
+  return (
+    <table class={styles.transactionTable}>
+      <thead>
+        <tr class={styles.tableHeader}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-  <tbody>
-      {items.map(item => (
+      <tbody>
+        {items.map((item) => (
           <tr class={styles.tableEl} key={item.id}>
             <td class={styles.type}>{firstUpper(item.type)}</td>
             <td>{item.amount}</td>
             <td class={styles.type}>{item.currency}</td>
-         </tr> 
-      ))}
-  </tbody>
-</table>
-    )
-}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 TransactionHistory.propTypes = {
-    id: PropTypes.string.isRequired,
-    type: PropTypes.string,
-    amount: PropTypes.string.isRequired,
-    currency: PropTypes.string,
-}
-   
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string,
+};
 
-TransactionHistory.defaultProps = { 
-    type: 'Type not found',
-    currency: 'Currency type not found'
-}
+TransactionHistory.defaultProps = {
+  type: "Type not found",
+  currency: "Currency type not found",
+};
 
-export default TransactionHistory
+export default TransactionHistory;
